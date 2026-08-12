7747330b2090fdc892ed4278a293dca8df6d0b75
fuureya <agiljibrin009@gmail.com>
2026-08-12 14:41:35 +0800

    chore: comment out forced HTTPS scheme and Vite prefetching in AppServiceProvider

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
-        Vite::prefetch(concurrency: 3);
+        // \Illuminate\Support\Facades\URL::forceScheme('https');
+        // Vite::prefetch(concurrency: 3);
     }
 }
